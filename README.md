a3f19229a663a3cb49ec2441e88c3505fab3befb
vinay chandrasekharan <vinay.cn@gmail.com>
2024-10-27 10:05:32 -0400

    completed all chapters

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Email: user@nextmail.com
+Password: 123456
+
 ## Next.js App Router Course - Starter
 
 This is the starter template for the Next.js App Router Course. It contains the starting code for the dashboard application.
--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,7 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import CardWrapper from '@/app/ui/dashboard/cards';
 import { lusitana } from '@/app/ui/fonts';
-import {  fetchLatestInvoices,fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton,LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
